318ff03958071f8bba5bc0854c780419a15febb8
Assaf Gelber <assaf.gelber@gmail.com>
2016-04-01 23:18:26 +0300

    Fix database url

--- config/environments.rb
+++ config/environments.rb
@@ -1,5 +1,5 @@
 configure :development, :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///development')
+ db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{ENV['RACK_ENV']}")
 
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
--- config/puma.rb
+++ config/puma.rb
@@ -9,5 +9,5 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || "postgres://localhost/#{ENV['RACK_ENV']}")
 end
