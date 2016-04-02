c9da011df060445edb7375144b4a6d822d96f776
Assaf Gelber <assaf.gelber@gmail.com>
2016-04-02 04:39:24 +0300

    More commands specs

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -7,9 +7,60 @@ describe Commands do
   end
 
   describe '::start' do
+    it 'retuns an error when there is already a run' do
+      run = Run.create(team_id: 'T1', channel_id: 'C1', user_id: 'U1', runner: 'runner')
+      Commands.send(:start, run, 'T1', 'C1', 'U1', 'runner', 1).must_equal({text: I18n.t('commands.start.already_on_run', name: run.runner)})
+    end
+
+    describe 'when no time is given' do
+      it 'returns a confirmation' do
+        expected_text = I18n.t('commands.start.success', name: 'runner')
+        Commands.send(:start, nil, 'T1', 'C1', 'U1', 'runner', nil).must_equal({text: expected_text, response_type: 'in_channel'})
+      end
+
+      it 'creates a run' do
+        Commands.send(:start, nil, 'T1', 'C1', 'U1', 'runner', nil)
+        Run.all.count.must_equal 1
+      end
+    end
+
+    describe 'when a time is given' do
+      it 'returns a confirmation' do
+        expected_text = I18n.t('commands.start.success_with_time', name: 'runner', time: 3)
+        Commands.send(:start, nil, 'T1', 'C1', 'U1', 'runner', 3).must_equal({text: expected_text, response_type: 'in_channel'})
+      end
+
+      it 'creates a run' do
+        Commands.send(:start, nil, 'T1', 'C1', 'U1', 'runner', 3)
+        Run.all.count.must_equal 1
+      end
+    end
   end
 
   describe '::order' do
+    it 'retuns an error when there is no run' do
+      Commands.send(:order, nil, 'U2', 'orderer', 'latte').must_equal({text: I18n.t('commands.order.no_run')})
+    end
+
+    it 'returns an error when there is no item' do
+      run = Run.create(team_id: 'T1', channel_id: 'C1', user_id: 'U1', runner: 'runner')
+      Commands.send(:order, run, 'U2', 'orderer', nil).must_equal({text: I18n.t('commands.order.order_missing', name: run.runner)})
+    end
+
+    describe 'when all is well' do
+      before do
+        @run = Run.create(team_id: 'T1', channel_id: 'C1', user_id: 'U1', runner: 'runner')
+      end
+
+      it 'returns a confirmation' do
+        Commands.send(:order, @run, 'U2', 'orderer', 'latte').must_equal({text: I18n.t('commands.order.success', item: 'latte')})
+      end
+
+      it 'creates an order' do
+        Commands.send(:order, @run, 'U2', 'orderer', 'latte')
+        @run.orders.count.must_equal 1
+      end
+    end
   end
 
   describe '::list' do
