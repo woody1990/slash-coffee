8b54b6e9236bce72e4dfa2321aa61ed07e7194fd
Assaf Gelber <assaf.gelber@gmail.com>
2016-04-03 15:00:30 +0300

    Fix get -> post

--- app.rb
+++ app.rb
@@ -5,15 +5,19 @@ require './config/i18n'
 require './config/environments'
 require './app/commands'
 
-get '/coffee' do
+post '/coffee' do
   content_type :json
 
-  team_id = params['team_id']
-  channel_id = params['channel_id']
-  user_id = params['user_id']
-  user_name = params['user_name']
-  text = params['text']
-  Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
+  if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
+    team_id = params['team_id']
+    channel_id = params['channel_id']
+    user_id = params['user_id']
+    user_name = params['user_name']
+    text = params['text']
+    Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
+  else
+    {text: "Nope"}.to_json
+  end
 end
 
 get '/authed' do
