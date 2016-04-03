d7db7f8a0c2736ea03a6d312527462be0c1e3dbb
Assaf Gelber <assaf.gelber@gmail.com>
2016-04-03 14:59:33 +0300

    Remove slack verification

--- app.rb
+++ app.rb
@@ -8,16 +8,12 @@ require './app/commands'
 get '/coffee' do
   content_type :json
 
-  if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
-    team_id = params['team_id']
-    channel_id = params['channel_id']
-    user_id = params['user_id']
-    user_name = params['user_name']
-    text = params['text']
-    Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
-  else
-    {text: "Nope"}.to_json
-  end
+  team_id = params['team_id']
+  channel_id = params['channel_id']
+  user_id = params['user_id']
+  user_name = params['user_name']
+  text = params['text']
+  Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
 end
 
 get '/authed' do
